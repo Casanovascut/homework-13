867bc0b7dfc970c43c6e3cba40dedffcefa01d75
Sebastian <smcasanova17@hotmail.com>
2023-01-10 16:13:45 -0500

    first commit project completed. finished alot quicker than i thought

---
+++ readme.md
@@ -0,0 +1,44 @@
+# ORM E-Commerce Back End website
+
+## Table of Contents
+
+* [Description](#description)
+
+* [License](#license)
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [Contributing](#contributing)
+
+* [Testing](#testing)
+
+* [Contact](#contact)
+
+## Description
+
+the task is to build a back end for an e-commerce site
+
+## License
+None
+
+## Installation
+
+intstall sequelize to interact with MySQL and express.js
+
+## Usage
+
+
+
+## contributing
+open
+
+## Testing
+
+none
+
+## Contact
+
+casanovascuts
+smcasanova17@hotmail.com
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,37 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const proData = await Product.findAll({
+      include: [{ model: Category },{ model: Tag}],
+    });
+    res.status(200).json(proData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id',async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const proData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category },{ model: Tag}],
+    });
+
+    if (!proData) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+
+    res.status(200).json(proData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -20,7 +42,7 @@ router.post('/', (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
-      price: 200.00,
+      price: 200.00,  
       stock: 3,
       tagIds: [1, 2, 3, 4]
     }
